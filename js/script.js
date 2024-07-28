947a254bfd422eeafa123972329c24f92ba4e429
Dave-N9ne <sergeytretiakov2003@gmail.com>
2024-07-28 18:11:26 +0300

    new

--- js/script.js
+++ js/script.js
@@ -4,5 +4,5 @@ const nav = document.querySelector('#header-nav');
 button.addEventListener ('click', function (event) {
     button.classList.toggle('_active');
     nav.classList.toggle('_active');
-    document.body.classList.toggle('_lock')
+    document.body.classList.toggle('_lock');
 })
