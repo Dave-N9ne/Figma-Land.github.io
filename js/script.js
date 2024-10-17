1ea7879dc18464deba715eb49ebbb4ba77c017dc
Dave-N9ne <sergeytretiakov2003@gmail.com>
2024-10-17 20:32:32 +0300

    add gallery section and connect slider swiper for this one

--- js/script.js
+++ js/script.js
@@ -6,3 +6,53 @@ button.addEventListener ('click', function (event) {
     nav.classList.toggle('_active');
     document.body.classList.toggle('_lock');
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+    const resizableSwiper = (breakPoint,
+                             swiperClass, 
+                             swiperSettings, 
+                             callback) => {
+        let swiper;
+        breakPoint = matchMedia(breakPoint);
+        const enableSwiper = (className, settings) => {
+            swiper = new Swiper(className, settings);
+            if (callback) {
+                callback(swiper);
+            }
+        }
+        const checker = () => {
+            if (breakPoint.matches) {
+                const result = enableSwiper(swiperClass, swiperSettings);
+                return result;
+            }
+            else {
+                if (swiper !== undefined) {
+                swiper.destroy(true, true);
+            }
+            return;
+            }
+        }
+        breakPoint.addEventListener('change', checker);
+        checker();
+    }
+    const foo = (instance) => {
+        if (instance) {
+            instance.on('slideChange', (event) => {
+                console.log('mySwiper.activeIndex', instance.activeIndex);
+            });
+        }
+    };
+    resizableSwiper (
+        '(max-width: 767.98px)',
+        '.swiper',
+        {
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                dynamicBullets: true
+               },
+            spaceBetween: 90
+        },
+        foo
+    )
+})
